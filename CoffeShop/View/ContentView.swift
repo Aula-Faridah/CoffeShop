ea114d4b9a1fd01d114121f39f3afa1c32cc216c
Aula Faridah <aulafaridah@gmail.com>
2024-04-17 13:57:11 +0700

    Coffeeshop View

--- CoffeShop/View/ContentView.swift
+++ CoffeShop/View/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    var coffee: [Coffeeshop] = CoffeeshopProvider.allData()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List(coffee) {coffee in
+                CoffeeshopItem(coffee: coffee)
+                    .listRowSeparator(.hidden)
+            }
+            .navigationTitle("Coffeeeshop")
+            .listStyle(.grouped)
         }
-        .padding()
     }
 }
 
